faae53e8df3e23732d6e7290e2ae89abfe67ee48
Randall Eike <randall@eikeconsulting.com>
2018-11-14 21:01:40 -0800

    Add project files.

---
+++ OneRosterSync.Net/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OneRosterSync.Net.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
