bb0d8d3f3810b0fa9bcbd05e6df7c1e3d05221b2
Randall Eike <randall@eikeconsulting.com>
2018-12-08 18:19:33 -0800

    DataSync and District controllers.

---
+++ OneRosterSync.Net/Controllers/DataSyncController.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OneRosterSync.Net.Data;
+using OneRosterSync.Net.Models;
+using OneRosterSync.Net.Processing;
+
+namespace OneRosterSync.Net.Controllers
+{
+    public class WhatsNewViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext db;
+
+        public WhatsNewViewComponent(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        public IViewComponentResult Invoke(District district)
+        {
+            //District district = db.Districts.Find(districtId);
+            var model = db.DataSyncHistories
+                .Where(history => history.DistrictId == district.DistrictId)
+                .OrderByDescending(h => h.Modified)
+                .Take(20)
+                .ToList();
+
+            return View(viewName: "Histories", model: model);
+        }
+    }
+
+    public class DataSyncController : Controller
+    {
+        private readonly ApplicationDbContext db;
+        private readonly IBackgroundTaskQueue TaskQueue;
+
+        public DataSyncController(
+            IBackgroundTaskQueue taskQueue,
+            ApplicationDbContext db)
+        {
+            TaskQueue = taskQueue;
+            this.db = db;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult DistrictList()
+        {
+            return View(db.Districts.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult DistrictCreate()
+        {
+            return View();
+        }
+
+        private IActionResult RedirectToDistrict(int districtId) =>
+            RedirectToAction(nameof(DistrictEdit), new { districtId });
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> DistrictCreate([Bind("Name")] District district)
+        {
+            if (!ModelState.IsValid)
+                return View(district);
+
+            db.Add(district);
+            await db.SaveChangesAsync();
+            return RedirectToDistrict(district.DistrictId);
+        }
+
+        [HttpGet]
+        public IActionResult DistrictEdit(int districtId)
+        {
+            District district = db.Districts.Find(districtId);
+            if (district == null)
+                return NotFound($"District {districtId} not found");
+
+            return View(district);
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> DistrictEdit(District district)
+        {
+            if (!ModelState.IsValid)
+                return View(district);
+
+            if (district.DailyProcessingTime.HasValue)
+            {
+                TimeSpan t = district.DailyProcessingTime.Value;
+                TimeSpan min = new TimeSpan(0);
+                TimeSpan max = new TimeSpan(hours: 24, minutes: 0, seconds: 0);
+                if (t < min || t > max)
+                {
+                    ModelState.AddModelError(
+                        key: nameof(district.DailyProcessingTime), 
+                        errorMessage: "Invalid Daily Processing Time.  Please enter a time between 0:0:0 and 23:59:59.  Or clear to disable daily processing.");
+                    return View(district);
+                }
+            }
+
+            District d = db.Districts.Find(district.DistrictId);
+
+            d.Name = district.Name;
+            d.DailyProcessingTime = district.DailyProcessingTime;
+            d.Touch();
+
+            await db.SaveChangesAsync();
+
+            return RedirectToDistrict(district.DistrictId);
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> DistrictProcess(District district)
+        {
+            District d = db.Districts.Find(district.DistrictId);
+            //d.NextProcessingTime = DateTime.UtcNow.AddSeconds(1);
+            d.ProcessingStatus = ProcessingStatus.ManuallyScheduled;
+            d.Touch();
+            await db.SaveChangesAsync();
+
+            return RedirectToDistrict(district.DistrictId);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DataSyncLines(int districtId)
+        {
+            var model = await db.DataSyncLines
+                .Where(l => l.DistrictId == districtId)
+                .OrderByDescending(l => l.LastSeen)
+                .Take(20)
+                .ToListAsync();
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult HistoryInfo(int dataSyncHistoryId)
+        {
+            var model = db.DataSyncHistories
+                .Include(h => h.District)
+                .SingleOrDefault(h => h.DataSyncHistoryId == dataSyncHistoryId);
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> HistoryDetailsList(int dataSyncHistoryId)
+        {
+            var model = await db.DataSyncHistoryDetails
+                .Where(d => d.DataSyncHistoryId == dataSyncHistoryId) 
+                .OrderByDescending(d => d.Modified)
+                .Take(20)
+                .ToListAsync();
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SelectCourses(int districtId)
+        {
+            var model = await db.DataSyncLines
+                .Where(l => l.DistrictId == districtId && l.Table == "CsvCourse")
+                .ToListAsync();
+
+            return View(model);
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> SelectCourses(int districtId, IEnumerable<string> SelectedCourses)
+        {
+            var model = await db.DataSyncLines
+                .Where(l => l.DistrictId == districtId && l.Table == "CsvCourse")
+                .ToListAsync();
+
+            foreach (var course in model)
+            {
+                bool include = SelectedCourses.Contains(course.SourceId);
+                if (course.IncludeInSync == include)
+                    continue;
+                course.IncludeInSync = include;
+                course.Touch();
+            }
+
+            await db.SaveChangesAsync();
+
+            return RedirectToDistrict(districtId);
+        }
+
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Apply(int districtId)
+        {
+            var query = db.DataSyncLines
+                .Where(l => l.DistrictId == districtId && l.SyncStatus == SyncStatus.ReadyToApply);
+
+            int i = 0;
+            foreach (var line in query)
+            {
+                // TODO call API and handle result
+                if (string.IsNullOrEmpty(line.TargetId))
+                    line.TargetId = Guid.NewGuid().ToString();
+
+                line.SyncStatus = SyncStatus.Applied;
+                line.Touch();
+
+                if (++i > 50)
+                {
+                    await db.SaveChangesAsync();
+                    i = 0;
+                }
+            }
+            await db.SaveChangesAsync();
+
+            return RedirectToDistrict(districtId);
+        }
+
+
+    }
+}
---
+++ OneRosterSync.Net/Controllers/DistrictsController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using OneRosterSync.Net.Data;
+using OneRosterSync.Net.Models;
+
+namespace OneRosterSync.Net.Controllers
+{
+    // Not used, delete this controller and views.  For reference....
+
+    public class DistrictsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DistrictsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Districts
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Districts.ToListAsync());
+        }
+
+        // GET: Districts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var district = await _context.Districts
+                .FirstOrDefaultAsync(m => m.DistrictId == id);
+            if (district == null)
+            {
+                return NotFound();
+            }
+
+            return View(district);
+        }
+
+        // GET: Districts/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Districts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("DistrictId,Name,DailyProcessingTime,NextProcessingTime,ProcessingStatus,Version,Created,Modified")] District district)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(district);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(district);
+        }
+
+        // GET: Districts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var district = await _context.Districts.FindAsync(id);
+            if (district == null)
+            {
+                return NotFound();
+            }
+            return View(district);
+        }
+
+        // POST: Districts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("DistrictId,Name,DailyProcessingTime,NextProcessingTime,ProcessingStatus,Version,Created,Modified")] District district)
+        {
+            if (id != district.DistrictId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(district);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DistrictExists(district.DistrictId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(district);
+        }
+
+        // GET: Districts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var district = await _context.Districts
+                .FirstOrDefaultAsync(m => m.DistrictId == id);
+            if (district == null)
+            {
+                return NotFound();
+            }
+
+            return View(district);
+        }
+
+        // POST: Districts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var district = await _context.Districts.FindAsync(id);
+            _context.Districts.Remove(district);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DistrictExists(int id)
+        {
+            return _context.Districts.Any(e => e.DistrictId == id);
+        }
+    }
+}
