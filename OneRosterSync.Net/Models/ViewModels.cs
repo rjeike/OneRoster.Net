b57aa936b0ef8905d3ebb84e0a3138c21eb18918
Randall Eike <randall@eikeconsulting.com>
2018-12-08 18:19:59 -0800

    Views and utilities

---
+++ OneRosterSync.Net/Models/ViewModels.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace OneRosterSync.Net.Models
+{
+}
---
+++ OneRosterSync.Net/Utils/Utilities.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OneRosterSync.Net.Utils
+{
+    public static class Utilities
+    {
+        public static string FormatJson(string json)
+        {
+            dynamic parsedJson = JsonConvert.DeserializeObject(json);
+            return JsonConvert.SerializeObject(parsedJson, Formatting.Indented);
+        }
+
+    }
+}
