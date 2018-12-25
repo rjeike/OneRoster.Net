6f34c42cdc29b719df3fbe02b7a5094d715bfce8
Randall Eike <randall@eikeconsulting.com>
2018-12-24 17:33:58 -0800

    Moved Extensions from Utils to Extensions folder. Added extension for the Serilog Logger to include additional context information with each entry.

--- OneRosterSync.Net/Extensions/CollectionExtensions.cs
+++ OneRosterSync.Net/Extensions/CollectionExtensions.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace OneRosterSync.Net.Utils
+namespace OneRosterSync.Net.Extensions
 {
     public static class CollectionExtensions
     {
---
+++ OneRosterSync.Net/Extensions/LoggerExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Runtime.CompilerServices;
+using Serilog.Context;
+
+namespace OneRosterSync.Net.Extensions
+{
+    public static class LoggerExtensions
+    {
+        /// <summary>
+        /// Adds information to the Serilog context, including:
+        /// 1. Member Name
+        /// 2. File Path
+        /// 3. Line Number
+        /// This is done at compile time for performance (rather than using reflection).
+        /// </summary>
+        public static ILogger Here(this ILogger logger,
+            [CallerMemberName] string memberName = "",
+            [CallerFilePath] string sourceFilePath = "",
+            [CallerLineNumber] int sourceLineNumber = 0)
+        {
+            LogContext.PushProperty("MemberName", memberName);
+            LogContext.PushProperty("FilePath", sourceFilePath);
+            LogContext.PushProperty("LineNumber", sourceLineNumber);
+
+            return logger;
+        }
+    }
+}
