15e44cc09a97bae0d718812a24fd46aee64af0b1
Randall Eike <randall@eikeconsulting.com>
2018-12-27 20:14:42 -0800

    Implemented the OneRoster.Net required APIs as MockAPIs

--- OneRosterSync.Net/Controllers/MockApiController.cs
+++ OneRosterSync.Net/Controllers/MockApiController.cs
@@ -8,11 +8,11 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using OneRosterSync.Net.Models;
 using OneRosterSync.Net.Extensions;
+using Newtonsoft.Json;
 
 namespace OneRosterSync.Net.Controllers
 {
-    [Route("api/mockapi/")]
-    [ApiController]
+    [ApiController, Route("api/mockapi/")]
     public class MockApiController : ControllerBase
     {
         private readonly ILogger Logger;
@@ -22,19 +22,56 @@ namespace OneRosterSync.Net.Controllers
             Logger = logger;
         }
 
-        // GET: api/MockApi/ping
         [HttpGet, Route("ping")]
         public IEnumerable<string> Ping()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // POST: api/MockApi/update
-        [HttpPost, Route("update")]
-        public JsonResult Update([FromBody] string data)
+        [HttpPost, Route("test")]
+        public string JsonStringBody([FromBody] string content)
         {
-            Logger.Here().LogInformation($"Recieved data from Mock API {data}");
-            return new JsonResult(new { myvar = data });
+            return content;
+        }
+
+        [HttpPost, Route("org")]
+        public async Task<JsonResult> Org([FromBody] ApiPost<CsvOrg> org) { return await ProcessEntity<CsvOrg>(org); }
+
+        [HttpPost, Route("course")]
+        public async Task<JsonResult> Course([FromBody] ApiPost<CsvCourse> course) { return await ProcessEntity<CsvCourse>(course); }
+
+        [HttpPost, Route("academicsession")]
+        public async Task<JsonResult> AcademicSession([FromBody] ApiPost<CsvAcademicSession> academicSession) { return await ProcessEntity<CsvAcademicSession>(academicSession); }
+
+        [HttpPost, Route("class")]
+        public async Task<JsonResult> Class([FromBody] ApiPost<CsvClass> _class) { return await ProcessEntity<CsvClass>(_class); }
+
+        [HttpPost, Route("user")]
+        public async Task<JsonResult> UserEntity([FromBody] ApiPost<CsvUser> user) { return await ProcessEntity<CsvUser>(user); }
+
+        [HttpPost, Route("enrollment")]
+        public async Task<JsonResult> Enrollment([FromBody] ApiPost<CsvEnrollment> enrollment) { return await ProcessEntity<CsvEnrollment>(enrollment); }
+
+
+        /// <summary>
+        /// Generic Handler for the MockAPIs
+        /// </summary>
+        private async Task<JsonResult> ProcessEntity<T>(ApiPost<T> entity) where T : CsvBaseObject
+        {
+            Logger.Here().LogInformation($"Recieved data from Mock API for {entity.EntityType}: {JsonConvert.SerializeObject(entity)}");
+
+            // simulate time to respond
+            await Task.Delay(100);
+
+            ApiResponse response = new ApiResponse
+            {
+                Success = true,
+                TargetId = entity.TargetId ?? System.Guid.NewGuid().ToString(),
+            };
+
+            Logger.Here().LogInformation($"Finished Processing data  for {entity.EntityType}.  Response: {JsonConvert.SerializeObject(response)}");
+
+            return new JsonResult(response);
         }
     }
 }
