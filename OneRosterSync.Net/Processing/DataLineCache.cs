f818380600e1d06ac9c2bc1e5a75e52914e1a9a4
Randall Eike <randall@eikeconsulting.com>
2018-12-29 15:02:16 -0800

    DataLineCache for loading all DataLines for a District and caching them in memory.

---
+++ OneRosterSync.Net/Processing/DataLineCache.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using OneRosterSync.Net.Extensions;
+using OneRosterSync.Net.Models;
+
+namespace OneRosterSync.Net.Processing
+{
+    public class DataLineCache
+    {
+        private Dictionary<string, Dictionary<string, DataSyncLine>> Cache = 
+            new Dictionary<string, Dictionary<string, DataSyncLine>>();
+
+        ILogger Logger;
+
+        public DataLineCache(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        public async Task Load(IQueryable<DataSyncLine> filter)
+        {
+            Cache.Clear();
+
+            Logger.Here().LogInformation("Begin Loading DataLineCache");
+
+            var sw = new Stopwatch();
+            sw.Start();
+
+            // could possibly use GroupBy instead, but this might be fine
+            string[] tables = await filter.Select(l => l.Table).Distinct().ToArrayAsync();
+            foreach (string table in tables)
+                Cache[table] = await filter
+                    .Where(l => l.Table == table)
+                    .ToDictionaryAsync(l => l.SourceId, l => l);
+
+            sw.Stop();
+            Logger.Here().LogInformation($"Done Loading DataLineCache.  It took {sw.ElapsedMilliseconds} milliseconds.");
+        }
+
+        public Dictionary<string, DataSyncLine> GetMap<T>() where T : CsvBaseObject
+        {
+            return Cache[nameof(T)];
+        }
+    }
+}
