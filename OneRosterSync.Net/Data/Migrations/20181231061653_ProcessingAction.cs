8bb6ae7803af74004fc666f429f582aac3ebe5d1
Randall Eike <randall@eikeconsulting.com>
2018-12-30 22:40:18 -0800

    Added ProcessingAction to the database

---
+++ OneRosterSync.Net/Data/Migrations/20181231061653_ProcessingAction.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OneRosterSync.Net.Data.Migrations
+{
+    public partial class ProcessingAction : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProcessingAction",
+                table: "Districts",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProcessingAction",
+                table: "Districts");
+        }
+    }
+}
