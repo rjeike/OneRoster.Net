c3349a38a6c6a9120c42b30fa3c7df83f055100c
Randall Eike <randall@eikeconsulting.com>
2019-01-03 18:21:58 -0800

    Fixing the return type in the MockAPI

--- OneRosterSync.Net/Controllers/MockApiController.cs
+++ OneRosterSync.Net/Controllers/MockApiController.cs
@@ -26,46 +26,46 @@ namespace OneRosterSync.Net.Controllers
         /// Organization updates
         /// </summary>
         [HttpPost, Route("org")]
-        public async Task<JsonResult> Org([FromBody] ApiPost<CsvOrg> org) { return await ProcessEntity<CsvOrg>(org); }
+        public async Task<ApiResponse> Org([FromBody] ApiPost<CsvOrg> org) { return await ProcessEntity<CsvOrg>(org); }
 
         /// <summary>
         /// Course updates
         /// </summary>
         [HttpPost, Route("course")]
-        public async Task<JsonResult> Course([FromBody] ApiPost<CsvCourse> course) { return await ProcessEntity<CsvCourse>(course); }
+        public async Task<ApiResponse> Course([FromBody] ApiPost<CsvCourse> course) { return await ProcessEntity<CsvCourse>(course); }
 
         /// <summary>
         /// Academic Session updates
         /// </summary>
         [HttpPost, Route("academicsession")]
-        public async Task<JsonResult> AcademicSession([FromBody] ApiPost<CsvAcademicSession> academicSession) { return await ProcessEntity<CsvAcademicSession>(academicSession); }
+        public async Task<ApiResponse> AcademicSession([FromBody] ApiPost<CsvAcademicSession> academicSession) { return await ProcessEntity<CsvAcademicSession>(academicSession); }
 
         /// <summary>
         /// Class updates
         /// </summary>
         [HttpPost, Route("class")]
-        public async Task<JsonResult> Class([FromBody] ApiPost<CsvClass> _class) { return await ProcessEntity<CsvClass>(_class); }
+        public async Task<ApiResponse> Class([FromBody] ApiPost<CsvClass> _class) { return await ProcessEntity<CsvClass>(_class); }
 
         /// <summary>
         /// User updates
         /// </summary>
         [HttpPost, Route("user")]
-        public async Task<JsonResult> UserEntity([FromBody] ApiPost<CsvUser> user) { return await ProcessEntity<CsvUser>(user); }
+        public async Task<ApiResponse> UserEntity([FromBody] ApiPost<CsvUser> user) { return await ProcessEntity<CsvUser>(user); }
 
         /// <summary>
         /// Enrollment updates
         /// Note that the LMS Target IDs for class and user are included in the EnrollmentMap object
         /// </summary>
         [HttpPost, Route("enrollment")]
-        public async Task<JsonResult> Enrollment([FromBody] ApiPost<CsvEnrollment> enrollment) { return await ProcessEntity<CsvEnrollment>(enrollment); }
+        public async Task<ApiResponse> Enrollment([FromBody] ApiPost<CsvEnrollment> enrollment) { return await ProcessEntity<CsvEnrollment>(enrollment); }
 
 
         /// <summary>
         /// Generic Handler for the MockAPIs
         /// </summary>
-        private async Task<JsonResult> ProcessEntity<T>(ApiPost<T> entity) where T : CsvBaseObject
+        private async Task<ApiResponse> ProcessEntity<T>(ApiPost<T> entity) where T : CsvBaseObject
         {
-            Logger.Here().LogInformation($"Recieved data from Mock API for {entity.EntityType}: {JsonConvert.SerializeObject(entity)}");
+            Logger.Here().LogInformation($"Recieved {entity.EntityType}: {JsonConvert.SerializeObject(entity)}");
 
             // simulate time to respond
             await Task.Delay(100);
@@ -76,9 +76,9 @@ namespace OneRosterSync.Net.Controllers
                 TargetId = entity.TargetId ?? System.Guid.NewGuid().ToString(),
             };
 
-            Logger.Here().LogInformation($"Finished Processing data  for {entity.EntityType}.  Response: {JsonConvert.SerializeObject(response)}");
+            Logger.Here().LogInformation($"Responding {entity.EntityType}: {JsonConvert.SerializeObject(response)}");
 
-            return new JsonResult(response);
+            return response;
         }
     }
 }
