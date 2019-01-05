39e4a03ad1d0045179b9fe7a515018e6d5c28cd5
Randall Eike <randall@eikeconsulting.com>
2019-01-04 16:09:56 -0800

    Added components to display Processing Stats on the Dashboard Updated Components for History, creating 2 views Updated reference in DistrictDashboard to use Tag Helpers

---
+++ OneRosterSync.Net/Components/DataSyncStats.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OneRosterSync.Net.Data;
+using OneRosterSync.Net.Models;
+using OneRosterSync.Net.Processing;
+
+namespace OneRosterSync.Net.Components
+{
+    public class DataSyncStats : ViewComponent
+    {
+        private readonly ILogger Logger;
+        private readonly ApplicationDbContext db;
+
+        public DataSyncStats(ApplicationDbContext db, ILogger<DataSyncStats> logger)
+        {
+            this.db = db;
+            Logger = logger;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int districtId)
+        {
+            var repo = new DistrictRepo(db, districtId);
+
+            var model = await OneRosterSync.Net.Controllers.DataSyncController.ReportLine(repo.Lines(), "");
+
+            return View(viewName:"Stats", model: model);
+        }
+    }
+}
