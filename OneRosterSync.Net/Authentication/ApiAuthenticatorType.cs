8f532eb41b7bb06ca28aa31c72f0f9598ebf8bff
ObaidurRehman <obaid.urrehman@gmail.com>
2019-01-15 19:54:17 +0500

    Rename ApiAuthenticationMode -> ApiAuthenticatorType

---
+++ OneRosterSync.Net/Authentication/ApiAuthenticatorType.cs
@@ -0,0 +1,25 @@
+﻿namespace OneRosterSync.Net.Authentication
+{
+	/// <summary>
+	/// Determines the type of authentication to use for the Target System
+	/// </summary>
+	public enum ApiAuthenticatorType
+	{
+		/// <summary>
+		/// No Api Authentication.
+		/// </summary>
+		None = 0,
+
+		/// <summary>
+		/// A simple Api key based authentication mode where the api key parameter is appended to the
+		/// target system api endpoint.
+		/// </summary>
+		ApiKey = 1,
+
+		/// <summary>
+		/// Basic Http Authentication. (Not implemented)
+		/// See: https://en.wikipedia.org/wiki/Basic_access_authentication
+		/// </summary>
+		HttpBasic = 2
+	}
+}
