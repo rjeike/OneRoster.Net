c79a1c431002a5c6b3bca03bcc66019dfe135004
Obaid <obaid@eikeconsulting.com>
2019-01-15 19:21:26 +0500

    Add Api Authentication Modes

---
+++ OneRosterSync.Net/Authentication/ApiKeyAuthenticator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Web;
+
+namespace OneRosterSync.Net.Authentication
+{
+	public class ApiKeyAuthenticator: IApiAuthenticator
+	{
+		private readonly string apiKey;
+		private readonly string apiKeyParameterName;
+
+		public ApiKeyAuthenticator(string apiKey, string apiKeyParameterName = "apiKey")
+		{
+			this.apiKey = apiKey;
+			this.apiKeyParameterName = apiKeyParameterName;
+		}
+
+		public void AddAuthentication(HttpClient client)
+		{
+			var uriBuilder = new UriBuilder(client.BaseAddress);
+			var query = HttpUtility.ParseQueryString(client.BaseAddress.Query);
+			query.Add(apiKeyParameterName, apiKey);
+
+			uriBuilder.Query = query.ToString();
+			client.BaseAddress = uriBuilder.Uri;
+		}
+	}
+}
---
+++ OneRosterSync.Net/Authentication/IApiAuthenticator.cs
@@ -0,0 +1,9 @@
+﻿using System.Net.Http;
+
+namespace OneRosterSync.Net.Authentication
+{
+    public interface IApiAuthenticator
+    {
+	    void AddAuthentication(HttpClient client);
+    }
+}
