35d2d4dd7e1ae7edb9feaf4dda7ef63be4dedef2
ObaidurRehman <obaid.urrehman@gmail.com>
2019-01-16 13:36:27 +0500

    Add Api Authentication to the outgoing LMS request

--- OneRosterSync.Net/Authentication/IApiAuthenticator.cs
+++ OneRosterSync.Net/Authentication/IApiAuthenticator.cs
@@ -4,6 +4,11 @@ namespace OneRosterSync.Net.Authentication
 {
     public interface IApiAuthenticator
     {
+		/// <summary>
+		/// Modify the client so that default authorization header or api query key parameters are
+		/// added to the outgoing request.
+		/// </summary>
+		/// <param name="client"></param>
 	    void AddAuthentication(HttpClient client);
     }
 }
