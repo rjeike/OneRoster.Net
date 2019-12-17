29fa119163469da8d5608b16f4ebef3e248e277e
Sandesh Kumar <sandeshkumar@folio3.com>
2019-12-17 18:26:55 +0500

    hangfire nightly sync

---
+++ OneRosterSync.Net/Processing/HangfireNightlySyncScheduler.cs
@@ -0,0 +1,33 @@
+﻿using Hangfire;
+using Hangfire.Annotations;
+using Hangfire.Dashboard;
+using Hangfire.SqlServer;
+using OneRosterSync.Net.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace OneRosterSync.Net.Processing
+{
+    public class HangfireNightlySyncScheduler
+    {
+        public static void ScheduleNightlySync(string conString)
+        {
+            JobStorage.Current = new SqlServerStorage(conString);
+            var CSTZone = TimeZoneInfo.FindSystemTimeZoneById("Central Standard Time");
+            if (CSTZone != null)
+            {
+                RecurringJob.RemoveIfExists(nameof(NightlyFtpSyncService));
+                RecurringJob.AddOrUpdate<NightlyFtpSyncService>(nameof(NightlyFtpSyncService), job => job.Run(JobCancellationToken.Null),
+                    Cron.Daily(1), CSTZone);
+                //RecurringJob.AddOrUpdate<NightlyFtpSyncService>(nameof(NightlyFtpSyncService), job => job.Run(JobCancellationToken.Null),
+                //    Cron.Daily(7, 20), CSTZone);
+                //RecurringJob.AddOrUpdate<NightlyFtpSyncService>(nameof(NightlyFtpSyncService), job => job.Run(JobCancellationToken.Null),
+                //   $"*/{10} * * * *", TimeZoneInfo.Local);
+            }
+        }
+    }
+
+}
---
+++ OneRosterSync.Net/Processing/NightlyFtpSyncService.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hangfire;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using OneRosterSync.Net.Controllers;
+using OneRosterSync.Net.Data;
+using OneRosterSync.Net.Models;
+
+namespace OneRosterSync.Net.Processing
+{
+    public interface INightlyFtpSyncService
+    {
+        Task RunNightlyFtpSync();
+    }
+    public class NightlyFtpSyncService : INightlyFtpSyncService
+    {
+        //private readonly ILogger Logger = ApplicationLogging.Factory.CreateLogger<NightlyFtpSyncService>();
+        //private readonly ApplicationDbContext _db;
+
+        //public NightlyFtpSyncService(ApplicationDbContext db)
+        //{
+        //    //Db = ServiceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        //    _db = db;
+        //}
+
+        private readonly ApplicationDbContext _db;
+        private readonly ILogger<DataSyncController> _logger;
+        private readonly IHostingEnvironment _hostingEnvironment;
+        public NightlyFtpSyncService(ApplicationDbContext db, ILogger<DataSyncController> logger, IHostingEnvironment hostingEnvironment)
+        {
+            _db = db; _logger = logger; _hostingEnvironment = hostingEnvironment;
+        }
+
+        public async Task Run(IJobCancellationToken token)
+        {
+            try
+            {
+                token.ThrowIfCancellationRequested();
+                await RunNightlyFtpSync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error running nightly sync: {ex.Message}.");
+            }
+        }
+
+        public async Task RunNightlyFtpSync()
+        {
+            try
+            {
+                DataSyncController controller = new DataSyncController(_db, _logger, _hostingEnvironment);
+                await controller.LoadFiles(_logger);
+                var districts = _db.Districts.Where(w => w.NightlySyncEnabled).ToList();
+                foreach (var district in districts)
+                {
+                    try
+                    {
+                        //await controller.Load(district.DistrictId);
+                        //await controller.IncludeInSyncOrgsNightlySync(district.DistrictId);
+                        //await controller.Analyze(district.DistrictId);
+                        //await controller.Apply(district.DistrictId);
+                        await controller.FullProcess(district.DistrictId);
+                    }
+                    catch (Exception ex)
+                    {
+                        _logger.LogError(ex, $"Error processing in nightly sync for district: {district.DistrictId}. Error: {ex.Message}.");
+                        throw ex;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
