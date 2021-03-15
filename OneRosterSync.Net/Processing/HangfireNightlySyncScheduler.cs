d2128d2c167c3325b0cbbafec6fa25c2f03f57a0
sandesh_folio3 <sandeshkumar@folio3.com>
2021-03-15 14:53:32 +0500

    updated cron expression to 6 cst

--- OneRosterSync.Net/Processing/HangfireNightlySyncScheduler.cs
+++ OneRosterSync.Net/Processing/HangfireNightlySyncScheduler.cs
@@ -40,7 +40,7 @@ namespace OneRosterSync.Net.Processing
                 });
 
                 RecurringJob.AddOrUpdate<NightlyFtpSyncService>(nameof(NightlyFtpSyncService), job => job.SendConsolidatedSyncErrorsEmail(JobCancellationToken.Null),
-                   "0 1 * * *", CSTZone);
+                   "0 6 * * *", CSTZone);
                 //RecurringJob.AddOrUpdate<NightlyFtpSyncService>(nameof(NightlyFtpSyncService), job => job.Run(JobCancellationToken.Null),
                 //   Cron.Daily(1), CSTZone);
                 //RecurringJob.AddOrUpdate<NightlyFtpSyncService>(nameof(NightlyFtpSyncService), job => job.Run(JobCancellationToken.Null),
